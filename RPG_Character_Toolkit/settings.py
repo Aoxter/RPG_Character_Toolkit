8827da2ff96a50dd4c2c1fd28641b09659c07eb5
Piotr Kopycki <pkopycki@onet.pl>
2021-10-10 11:26:28 +0200

    Profiles system

--- RPG_Character_Toolkit/settings.py
+++ RPG_Character_Toolkit/settings.py
@@ -131,6 +131,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "RPG_Character_Toolkit/static")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'characters/static'), )
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
